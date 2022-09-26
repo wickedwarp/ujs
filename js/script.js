77a5b4dd971d704ced8519bb22de7266478f0d6d
wicked_warp <sulitsa88@gmail.com>
2022-09-26 16:55:33 +0300

    second task

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?')
+'use strict';
 
-let personalMovieDB = {
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    const b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
 
-let a = prompt('Один из последних просмотренных фильмов?')
-    b = prompt('На сколько его оцените?')
-    c = prompt('Один из последних просмотренных фильмов?')
-    d = prompt('На сколько его оцените?')
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
-
-console.log(personalMovieDB)
-
+console.log(personalMovieDB);
