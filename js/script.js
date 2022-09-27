482fab788748a05f740b722801c17e72ca35fe0c
wicked_warp <sulitsa88@gmail.com>
2022-09-27 10:48:04 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,26 +11,26 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-    const b = prompt('На сколько оцените его?', '');
+    const a = prompt('Один из последних просмотренных фильмов?', '')
+    const b = prompt('На сколько оцените его?', '')
 
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
+        personalMovieDB.movies[a] = b
+        console.log('done')
     } else {
-        console.log('error');
-        i--;
+        console.log('error')
+        i--
     }
 }
 
 if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
+    console.log("Просмотрено довольно мало фильмов")
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
+    console.log("Вы классический зритель")
 } else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
+    console.log("Вы киноман")
 } else {
-    console.log("Произошла ошибка");
+    console.log("Произошла ошибка")
 }
 
-console.log(personalMovieDB);
+console.log(personalMovieDB)
